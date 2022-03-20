15298f39ff4bd1e03e6cd49669f8c203554b56ee
Erik Fortune <pc@erik.org>
2022-03-20 14:49:09 -0700

    docs for extendedArray

--- src/extendedArray.ts
+++ src/extendedArray.ts
@@ -22,18 +22,43 @@
 
 import { Result, fail, succeed } from './result';
 
+/**
+ * An experimental array template which extend built-in `Array` to include a handful
+ * of predicates which return {@link Result | Result<T>}.
+ * @beta
+ */
 export class ExtendedArray<T> extends Array<T> {
     public readonly itemDescription: string;
 
+    /**
+     * Constructs an {@link ExtendedArray}.
+     * @param itemDescription - Brief description of the type of each item in this array.
+     * @param items - The initial contents of the array.
+     */
     public constructor(itemDescription: string, ...items: T[]) {
         super(...items);
         this.itemDescription = itemDescription;
     }
 
+    /**
+     * Type guard to determine if some arbitrary array is an
+     * {@link ExtendedArray}
+     * @param a - The `Array` to be tested.
+     * @returns Returns `true` if `a` is an {@link ExtendedArray},
+     * `false` otherwise.
+     */
     public static isExtendedArray<T>(a?: T[]): a is ExtendedArray<T> {
         return a instanceof ExtendedArray;
     }
 
+    /**
+     * Determines if this array contains exactly one element which matches
+     * a supplied predicate.
+     * @param predicate - The predicate function to be applied.
+     * @returns Returns {@link Success | Success<T>} with the single matching
+     * result if exactly one item matches `predicate`.  Returns {@link Failure}
+     * with an error message if there are no matches or more than one match.
+     */
     public single(predicate?: (item: T) => boolean): Result<T> {
         const match = (predicate ? this.filter(predicate) : this);
         if (match.length === 1) {
@@ -45,6 +70,13 @@ export class ExtendedArray<T> extends Array<T> {
         return fail(`${this.itemDescription} matches ${match.length} items`);
     }
 
+    /**
+     * Returns the first element of an {@link ExtendedArray}. Fails with an
+     * error message if the array is empty.
+     * @param failMessage - Optional message to be displayed in the event of failure.
+     * @returns Returns {@link Success | Success<T>} with the value of the first element
+     * in the array, or {@link Failure} with an error message if the array is empty.
+     */
     public first(failMessage?: string): Result<T> {
         if (this.length > 0) {
             return succeed(this[0]);
@@ -52,6 +84,14 @@ export class ExtendedArray<T> extends Array<T> {
         return fail(failMessage ?? `${this.itemDescription} not found`);
     }
 
+    /**
+     * Returns an array containing all elements of an {@link ExtendedArray}. Fails with
+     * an error message if the array is empty.
+     * @param failMessage - Optional message to be displayed in the event of failure.
+     * @returns Returns {@link Success | Success<T[]>} with a new (non-extended) `Array`
+     * containing the elements of this array, or {@link Failure} with an error message
+     * if the array is empty.
+     */
     public atLeastOne(failMessage?: string): Result<T[]> {
         if (this.length > 0) {
             return succeed(Array.from(this));
@@ -59,6 +99,11 @@ export class ExtendedArray<T> extends Array<T> {
         return fail(failMessage ?? `${this.itemDescription} not found`);
     }
 
+    /**
+     * Gets a new (non-extended) `Array` containing all of the elements from this
+     * {@link ExtendedArray}.
+     * @returns A new (non-extended) `Array<T>`.
+     */
     public all(): T[] {
         return Array.from(this);
     }
